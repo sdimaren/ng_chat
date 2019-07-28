dff47434c103ee6a0c5e8b1f5e9b47b46f1aed00
Santiago Dimaren <sdimaren1@gmail.com>
2019-07-28 11:21:34 -0400

    adding the navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ChatComponent } from './pages/chat/chat.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent },
+  { path: 'chat', component: ChatComponent },
   { path: '**', redirectTo: '/login'}
 ];
 
