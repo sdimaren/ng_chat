02f44576183daf42d56632f411634ee83ea515d7
Santiago Dimaren <sdimaren1@gmail.com>
2019-07-31 17:14:17 -0400

    improved styling

--- src/app/pages/chat/components/chat-window/chat-window.component.ts
+++ src/app/pages/chat/components/chat-window/chat-window.component.ts
@@ -19,6 +19,15 @@ export class ChatWindowComponent implements OnInit {
       }
     },
     { 
+      message: 'Ut eu elit sodales leo ultricies pulvinar. Cursus mattis quis at lacus. in elementum scelerisque, sem urna vulputate enim, ac posuere purus diam ac velit. Sed enim velit, condimentum nec tincidunt non, elementum sed nisi. Cras pharetra dui eu scelerisque pharetra.',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Kyle',
+        lastName: 'Brooks',
+        photoUrl: 'http://via.placeholder.com/150x150'
+      }
+    },
+    { 
       message: 'Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero.',
       createdAt: new Date(),
       sender: {
@@ -28,11 +37,20 @@ export class ChatWindowComponent implements OnInit {
       }
     },
     { 
+      message: 'Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero.',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Kyle',
+        lastName: 'Brooks',
+        photoUrl: 'http://via.placeholder.com/150x150'
+      }
+    },
+    { 
       message: 'Ut eu elit sodales leo ultricies pulvinar. Fusce iaculis magna gravida tempus congue. Ut sit amet nulla sed nisi cursus mattis quis at lacus. Proin commodo, justo in elementum scelerisque, sem urna vulputate enim, ac posuere purus diam ac velit. Sed enim velit, condimentum nec tincidunt non, elementum sed nisi. Cras pharetra dui eu scelerisque pharetra. Curabitur auctor feugiat nibh eget molestie. Duis scelerisque auctor mi, sit amet efficitur magna vulputate quis. Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero. Praesent eu tristique felis. Nunc vestibulum enim et justo dignissim lacinia nec et diam.',
       createdAt: new Date(),
       sender: {
         firstName: 'Sally',
-        lastName: 'Jones',
+        lastName: 'Brooks',
         photoUrl: 'http://via.placeholder.com/150x150'
       }
     },
@@ -41,10 +59,20 @@ export class ChatWindowComponent implements OnInit {
       createdAt: new Date(),
       sender: {
         firstName: 'Sally',
-        lastName: 'Jones',
+        lastName: 'Brooks',
+        photoUrl: 'http://via.placeholder.com/150x150'
+      },
+    },
+    { 
+      message: 'Sed enim velit, condimentum nec tincidunt non, elementum sed nisi.',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Kyle',
+        lastName: 'Brooks',
         photoUrl: 'http://via.placeholder.com/150x150'
       }
     }
+    
   ];
 
   constructor() {  }
