f44b839c74f9c309ec183e633112f57ec68174ec
Santiago Dimaren <sdimaren1@gmail.com>
2019-08-20 13:46:19 -0400

    fetching chatroom data based on route

--- src/app/pages/chat/components/chat-window/chat-window.component.ts
+++ src/app/pages/chat/components/chat-window/chat-window.component.ts
@@ -1,7 +1,8 @@
-import { ChatService } from './../../../../services/chat.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
+import { LoadingService } from './../../../../services/loading.service';
+import { ChatService } from './../../../../services/chat.service';
 
 @Component({
   selector: 'app-chat-window',
@@ -83,10 +84,24 @@ export class ChatWindowComponent implements OnInit, OnDestroy {
 
   constructor(
     private route: ActivatedRoute,
-    private chatService: ChatService
-  ) {  }
+    private chatService: ChatService,
+    private loadingService: LoadingService
+  ) { 
+    this.subscriptions.push(
+      this.chatService.selectedChatroom.subscribe(chatroom => {
+        this.chatroom = chatroom;
+        this.loadingService.isLoading.next(false);
+      })
+    );
+   }
 
   ngOnInit() {
+    this.subscriptions.push(
+      this.route.paramMap.subscribe(params => {
+        const chatroomId = params.get('chatroomId');
+        this.chatService.changeChatrooms.next(chatroomId);
+      })
+    )
   }
 
   ngOnDestroy() {
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,6 +1,8 @@
+import { switchMap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, of } from 'rxjs';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { LoadingService } from './loading.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,21 @@ import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firest
 export class ChatService {
 
   public chatrooms: Observable<any>;
+  public changeChatrooms: BehaviorSubject<string | null> = new BehaviorSubject(null);
+  public selectedChatroom: Observable<any>;
+  public selectedChatroomMessages: Observable<any>;
 
   constructor(
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private loadingService: LoadingService
   ) { 
+    this.selectedChatroom = this.changeChatrooms.pipe(switchMap(chatroomId => {
+      if (chatroomId) {
+        this.loadingService.isLoading.next(true);
+        return db.doc(`chatrooms/${chatroomId}`).valueChanges();
+      }
+      return of(null);
+    }))
     this.chatrooms = db.collection('chatrooms').valueChanges();
   }
 }
